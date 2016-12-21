86931eb8dc03f4927e1f53f233fef0cb296eb459
fsarigoz <fsarigoz@gmail.com>
2016-12-20 17:02:09 -0800

    First 3 articles

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,9 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Fatih Sarigoz'
-SITENAME = u'Fatih Sarigoz Data Science Blog'
-SITEURL = ''
+SITENAME = u'Fatih Sarigoz'
+SITESUBTITLE = u'Data Science Blog'
+SITEURL = ''  # Intentionally left blank, see ./publishconf.py
 
 PATH = 'content'
 
@@ -20,23 +21,47 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
+GITHUB_URL = 'http://github.com/fsarigoz'
+TWITTER_USERNAME = '@fatihsarigoz'
+
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/fatihsarigoz'),
+          ('linkedin', 'http://www.linkedin.com/in/fatihsarigoz'),
+          ('github', 'https://github.com/fsarigoz'),)
 
 DEFAULT_PAGINATION = 10
 
 MARKUP = ('md', 'ipynb')
 
-PLUGIN_PATH = './plugins'
-PLUGINS = ['ipynb.markup']
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup', 'pelican-bootstrapify']
 
+BOOTSTRAPIFY = {
+    'table': ['table', 'table-striped', 'table-hover'],
+    'img': ['img-fluid'],
+    'blockquote': ['blockquote'],
+}
+
+# Theme settings --------------------------------------------------------------
+THEME = u'flex'
+
+# Alchemy specific settings
+ICONS = (('twitter', 'https://twitter.com/fatihsarigoz'),
+          ('linkedin', 'http://www.linkedin.com/in/fatihsarigoz'),
+          ('github', 'https://github.com/fsarigoz'),)
+
+PYGMENTS_STYLE = 'default'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Flex specific settings
+MAIN_MENU = True
+MENUITEMS = (('Archives', '/archives.html'),)
+
 
 
